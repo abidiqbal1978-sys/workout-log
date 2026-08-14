d53c7b11c6a64e85fbf7bc2bda5ba2b0c9e7563a
abidiqbal1978-sys <abidiqbal1978@icloud.com>
2026-08-15 02:16:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'transformation-os-v6';
+const CACHE = 'transformation-os-v7';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
